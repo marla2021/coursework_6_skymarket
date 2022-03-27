f3a188214a3fea3b87ce3db462d5d437d0721a61
marla2021 <91145453+marla2021@users.noreply.github.com>
2022-03-27 14:08:08 +0300

    add models ad/comment

--- skymarket/ads/models.py
+++ skymarket/ads/models.py
@@ -1,12 +1,19 @@
 from django.conf import settings
 from django.db import models
 
+from skymarket.users.models import User
+
 
 class Ad(models.Model):
-    # TODO добавьте поля модели здесь
-    pass
+    title = models.CharField(max_length=200, null=True)
+    price = models.IntegerField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(null=True)
+    description = models.CharField(max_length=1000, null=True)
 
 
 class Comment(models.Model):
-    # TODO добавьте поля модели здесь
-    pass
+    text = models.CharField(max_length=1000, null=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(null=True)
+    ad = models.ForeignKey(Ad, on_delete=models.CASCADE)
